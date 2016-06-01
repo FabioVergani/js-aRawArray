8998dec86d9a135c9d67e521e5dc58c9d00b2ffd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-01 14:34:05 +0200

    Update aRawArray.js

--- aRawArray.js
+++ aRawArray.js
@@ -1,7 +1,8 @@
 (function(){
  function aRawArray(i){return setProto(Array(i),RawArrayProto);};
 
- const RawArrayProto=Object.setPrototypeOf([],null);
+ const setProto=Object.setPrototypeOf;
+ const RawArrayProto=setProto([],null);
 
  var a=aRawArray();
  var b=aRawArray(9);
@@ -10,3 +11,4 @@
  console.log('a',a,a.length,'b',b,b.length);
 
 })();
+
