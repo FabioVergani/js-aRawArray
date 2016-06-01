ca45bc3230f1c32975d31ba58167c31474f1c8fd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-01 15:38:54 +0200

    Update aRawArray-test.js

--- aRawArray-test.js
+++ aRawArray-test.js
@@ -3,12 +3,51 @@
 
  const setProto=Object.setPrototypeOf;
  const RawArrayProto=setProto([],null);
+ RawArrayProto.each=function(x){const m=this,l=m.length,f=x;var i=0;while(l>i){f(m[i],i++);};};
 
- var a=aRawArray();
- var b=aRawArray(9.2);
+ var A=aRawArray();
+
+ A['zzz']=666;
+ A[3]=1;
+ A[6]=1;
+ A[7]=2;
+
+ var B=[].concat(A);
+
+ function test(v,i){console.log(i,v)}
 
  console.clear();
- console.log('a',a,a.length,'b',b,b.length);
+
+
+ console.group('test A.each');
+ console.dir(A);
+ A.each(test);
+ console.groupEnd();
+
+ console.group('test B.forEach');
+ console.dir(B);
+ B.forEach(test);
+ console.groupEnd();
 
 })();
+/*
+
+test 
+
+ A.each/Array[8]
+ 0 undefined
+ 1 undefined
+ 2 undefined
+ 3 1
+ 4 undefined
+ 5 undefined
+ 6 1
+ 7 2
+
+ test B.forEach/Array[8] //It is not invoked for index properties that have been deleted or are uninitialized (i.e. on sparse arrays).
+ 3 1
+ 6 1
+ 7 2
+
+*/
 
